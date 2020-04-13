d7516577c9024fac5c5b9559e17653a2aa3ef477
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2020-04-13 21:54:14 +0300

    http02 teht'v't

--- Node.js/02 http-palvelin/Express.js
+++ Node.js/02 http-palvelin/Express.js
@@ -1,44 +1,47 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+const path = require('path');
 const port = 3000;
 
+app.use(express.urlencoded())
 app.use('/', express.static('./web-client/public'))
-
-
-
-app.get('/api/exercise?name', (req, res) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(
-        {
-            name: req.query.name, 
-            occupation: req.query.occupation,
-            placeofbirth: req.query.placeofbirth
-        }
-        ))
-});
-
-app.get('/api/exercise?number', (req, res) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(
-        {
-            number1: req.query.number1, 
-            number2: req.query.number2,
-        }
-        ))
-});
+app.use(express.json())
 
 app.get('/api/exercise', (req, res) =>{
     res.statusCode = 200
-    res.send('This is page without parameters!')
+    res.send(req.query)
 });
 
 app.listen(port, () =>
 console.log(`Example app listening on
 port ${port}!`))
 
-app.post('/users', function (req, res) {
-    res.send('Got a POST request')
-    })
+app.post('/api/exercise', function (req, res) {
+    res.statusCode = 200
+    res.send('Got a POST request') //yritetty ties kuinka kauan...
+    });
+
+app.post('/api/login', (req, res) =>{  
+    if (req.body.user || req.body.pwd == "") {
+        res.statusCode = 400;
+    }     
+    else if (req.body.user == "mark" && req.body.pwd == "giraffe") {
+        res.statusCode = 200;
+        res.send(JSON.stringify(
+            {
+                user: req.body.user, 
+            }
+            ))
+    } else {
+        res.statusCode = 403;
+    }
     
+    });
+
+app.get('/api', cors(), (req, res, next) => {
+    res.json({ msg: 'Hello, World!' })
+    });
+app.get('/hello', cors(), (req, res, next) => {
+    res.sendfile('./hello')
+    });
